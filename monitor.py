91c22c97127b9462ae58fafb6d5acb952f5221fd
Blake W Ford <blakewford@gmail.com>
2024-03-31 12:44:02 -0500

    Add mqtt class

--- monitor.py
+++ monitor.py
@@ -6,6 +6,34 @@ import time
 ssid = ''
 password = ''
 
+mqtt_port = 1883
+
+# Control packet type
+RESERVED=0,
+CONNECT=1,
+CONNACK=2,
+PUBLISH=3,
+PUBACK=4,
+PUBREC=5,
+PUBREL=6,
+PUBCOMP=7,
+SUBSCRIBE=8,
+SUBACK=9,
+UNSUBSCRIBE=10,
+UNSUBACK=11,
+PINGREQ=12,
+PINGRESP=13,
+DISCONNECT=14,
+#RESERVED=15
+
+class mqtt:
+    def connect(address, username, password):
+        return
+    def publish(topic, message):
+        return
+    def disconnect():
+        return
+
 def connect():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
@@ -16,6 +44,7 @@ def connect():
     return wlan.ifconfig()[0]
 
 ip = connect()
+mq = mqtt(address=ip,username='',password='')
 
 button = machine.Pin(26, machine.Pin.IN)
 adc = ADC(Pin(27))
@@ -28,5 +57,6 @@ while True:
         led.high()
     
     raw = adc.read_u16() & 0xFFF
-    print((raw/4095)*1023)
+    value = (raw/4095)*1023
+    print(value)
     time.sleep(1)
